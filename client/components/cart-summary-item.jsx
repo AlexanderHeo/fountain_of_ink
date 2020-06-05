3503011b3123b77021d91c9a39e12c362ec440ba
AlexanderHeo <jikster712@gmail.com>
2020-06-05 10:56:23 -0700

    changed flex-direction to row in shopping cart at smaller screens changed shop-name samller at smaller screens added bottom margins so button and text don't sit on bottom of screen

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -8,8 +8,6 @@ function CartSummaryItem(props) {
     const price = (parseInt(props.item.price) * 0.01).toFixed(2);
     const shortDescription = props.item.shortDescription;
     const id = props.item.productId;
-    const cartQuantity = props.cartQuantity;
-    const count = Object.values(cartQuantity).find(values => cartQuantity[id] === values);
     return (
       <div
         className="d-flex cart-item-container"
@@ -19,7 +17,6 @@ function CartSummaryItem(props) {
         </div>
         <div className="d-flex ml-3 cart-item-description">
           <h5 className="name">{name}</h5>
-          <div className="cartCount">x{count}</div>
           <div className="price">${price} each</div>
           <div className="short-description">{shortDescription}</div>
         </div>
