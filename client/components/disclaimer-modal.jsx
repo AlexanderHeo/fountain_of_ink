df402c0a38813cd804db8c7553b7add262a74fcb
AlexanderHeo <jikster712@gmail.com>
2020-10-02 14:53:19 -0700

    created disclaimer modal with close button

---
+++ client/components/disclaimer-modal.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const disclaimer = props => (
+  <div className="row" style={{
+    position: 'absolute', width: '100%', height: '100%', zIndex: '10', backgroundColor: '#333', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '26px', color: '#eee'
+  }}>
+    <div style={{
+      width: '50%', textAlign: 'center'
+    }}>
+      <div>
+        <p>This is a demo site. You cannot purchase anything. Please do not enter any real personal data in the checkout form.</p>
+      </div>
+      <button style={{
+        padding: '10px 20px',
+        borderRadius: '10px',
+        marginTop: '20px'
+      }} onClick={props.close}>Ok, I understand</button>
+    </div>
+  </div>
+);
+
+export default disclaimer;
