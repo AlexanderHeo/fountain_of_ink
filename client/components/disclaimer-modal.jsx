ca66f9939bcfacbc7444316e834e4a771d823b95
AlexanderHeo <jikster712@gmail.com>
2020-10-02 15:30:00 -0700

    fixed floating modal

--- client/components/disclaimer-modal.jsx
+++ client/components/disclaimer-modal.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 
+const buttonStyle = {
+  padding: '10px 20px', borderRadius: '10px', marginTop: '20px'
+};
+
 const disclaimer = props => (
-  <div className="row" style={{
-    position: 'absolute', width: '100%', height: '100%', zIndex: '10', backgroundColor: 'rgba(51, 51, 51, 0.9)', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '26px', color: '#eee'
-  }}>
-    <div style={{
-      width: '50%', textAlign: 'center', padding: '30px', backgroundColor: '#222', borderRadius: '20px'
-    }}>
-      <div>
+  <div className="row">
+    <div className="disclaimer-container">
+      <div className="disclaimer">
         <p>This is a demo site. You cannot purchase anything. Please do not enter any real personal data in the checkout form.</p>
+        <button style={buttonStyle} onClick={props.close}>Ok, I understand</button>
       </div>
-      <button style={{
-        padding: '10px 20px',
-        borderRadius: '10px',
-        marginTop: '20px'
-      }} onClick={props.close}>Ok, I understand</button>
     </div>
   </div>
 );
