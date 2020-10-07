ed374c006970a8a324fa72fb0879a24bd46ce813
AlexanderHeo <jikster712@gmail.com>
2020-10-07 10:12:54 -0700

    added noscroll to body when modal is showing added media queries to size modal on smaller screens

--- client/components/disclaimer-modal.jsx
+++ client/components/disclaimer-modal.jsx
@@ -1,39 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const discContainer = {
-  position: 'absolute',
-  top: '0',
-  left: '0',
-  height: '100%',
-  width: '100%',
-  zIndex: '100',
-  backgroundColor: 'rgba(51, 51, 51, 0.9)',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  fontSize: '26px',
-  color: '#eee'
-};
-const contentStyle = {
-  width: '50%',
-  textAlign: 'center',
-  padding: '30px',
-  backgroundColor: '#222',
-  borderRadius: '20px'
-};
-const buttonStyle = {
-  padding: '10px 20px', borderRadius: '10px', marginTop: '20px'
-};
+class Disclaimer extends Component {
+  componentDidMount() {
+    document.body.classList.add('noscroll');
+  }
 
-const disclaimer = props => (
-  <div className="row">
-    <div style={discContainer}>
-      <div style={contentStyle}>
-        <p>This is a demo site. You cannot purchase anything. Please do not enter any real personal data in the checkout form.</p>
-        <button autoFocus style={buttonStyle} onClick={props.close} >Ok, I understand</button>
+  componentWillUnmount() {
+    document.body.classList.remove('noscroll');
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className='discContainer'>
+          <div className='contentStyle'>
+            <p>This is a demo site. You cannot purchase anything. Please do not enter any real personal data in the checkout form.</p>
+            <button autoFocus className='buttonStyle' onClick={this.props.close} >Ok, I understand</button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
-export default disclaimer;
+export default Disclaimer;
