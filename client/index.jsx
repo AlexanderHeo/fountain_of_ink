8200b403f1a2f0cc7a5c453c6c89541c7f60bd8f
AlexanderHeo <jikster712@gmail.com>
2020-10-18 15:48:56 -0700

    fetches all products async via redux-thunk

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import App from './containers/app';
-import reducer from './store/reducers/viewReducer';
+import productReducer from './store/reducers/productsReducer';
+import viewReducer from './store/reducers/viewReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const reducer = combineReducers({
+  viewReducer: viewReducer,
+  productReducer: productReducer
+});
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
