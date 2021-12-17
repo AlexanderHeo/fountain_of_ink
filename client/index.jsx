e587ad1e644816bcf77b496071fb5f592ce45b87
Alexander Heo <jikster712@gmail.com>
2021-12-16 18:44:02 -0800

    removed unnecessary files

--- client/index.jsx
+++ client/index.jsx
@@ -1,28 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
-import thunk from 'redux-thunk';
 import App from './containers/app';
-import cartReducer from './store/reducers/cartReducer';
-import productDetailReducer from './store/reducers/productDetailReducer';
-import productReducer from './store/reducers/productsReducer';
-import viewReducer from './store/reducers/viewReducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const reducer = combineReducers({
-  viewReducer: viewReducer,
-  productReducer: productReducer,
-  productDetailReducer: productDetailReducer,
-  cartReducer: cartReducer
-});
-
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
