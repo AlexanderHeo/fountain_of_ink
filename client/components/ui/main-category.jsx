8a06ea174617066b1a791a2da7c1e3326faecd56
Alexander Heo <jikster712@gmail.com>
2021-12-18 11:59:12 -0800

    feat: move main category into main

--- client/components/ui/main-category.jsx
+++ client/components/ui/main-category.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
-const MainCategory = ({ title, categories }) => {
-  const classname = `main-category ${title} flex`;
+const MainCategory = ({ categories }) => {
+  const classname = `main-category ${categories.title} flex`;
   return (
     <div className={classname}>
       <div className='main-category-title flex'>
-        <h3>{title}</h3>
+        <h3>{categories.title}</h3>
       </div>
       <div className='main-category-list grid'>
-        {categories.map((category) => {
+        {categories.categories.map((category) => {
           return (
-            <div className='main-category-item-container flex' key={category}>
-              <div className='main-category-item flex'>
-                <div>{category}</div>
+            <div className='main-category-item-container' key={category.id}>
+              <div className='main-category-type'>{category.type}</div>
+              <div className='main-category-img-container flex'>
+                <img
+                  src={category.img}
+                  alt={category.alt}
+                  className='main-category-img'
+                />
               </div>
             </div>
           );
--- client/components/ui/main.jsx
+++ client/components/ui/main.jsx
@@ -5,65 +5,77 @@ import {
   BsChevronRight,
   BsChevronUp,
 } from 'react-icons/bs';
+import { accessories, inks, papers, pens } from '../lib/mainCategory';
+import Brands from './brands';
+import MainCategory from './main-category';
 
 const Main = ({ handleCategory, menudown }) => {
   return (
-    <div className='main flex'>
-      <div className='category-container'>
-        <div className='category-select'>
-          <div className='categories flex'>
-            <div className='category-title flex' onClick={handleCategory}>
-              <h3>Category</h3>
-              {menudown ? (
-                <BsChevronUp className='chevron' />
-              ) : (
-                <BsChevronDown className='chevron' />
-              )}
-            </div>
-            {menudown ? (
-              <div className='category-list'>
-                <div className='category'>Fountain Pens</div>
-                <div className='category'>Inks</div>
-                <div className='category'>Papers</div>
-                <div className='category'>Accessories</div>
+    <div className='main'>
+      <div className='main-container flex'>
+        <div className='category-container'>
+          <div className='category-select'>
+            <div className='categories flex'>
+              <div className='category-title flex' onClick={handleCategory}>
+                <h3>Category</h3>
+                {menudown ? (
+                  <BsChevronUp className='chevron' />
+                ) : (
+                  <BsChevronDown className='chevron' />
+                )}
               </div>
-            ) : null}
+              {menudown ? (
+                <div className='category-list'>
+                  <div className='category'>Fountain Pens</div>
+                  <div className='category'>Inks</div>
+                  <div className='category'>Papers</div>
+                  <div className='category'>Accessories</div>
+                </div>
+              ) : null}
+            </div>
+          </div>
+          <div className='category-image'>
+            <img
+              src='images/poster.jpg'
+              alt='fountain pen poster'
+              className='poster'
+            />
+            <img
+              src='images/ink-wreath.jpg'
+              alt='a wreath of ink swatches'
+              className='notebook'
+            />
           </div>
         </div>
-        <div className='category-image'>
-          <img
-            src='images/poster.jpg'
-            alt='fountain pen image'
-            className='poster'
-          />
-          <img
-            src='images/notebook.jpg'
-            alt='noteboook image'
-            className='notebook'
-          />
+        <div className='side-bar'>
+          <div className='featured-section'>
+            <div className='featured-title'>
+              <h3>Featured Pens</h3>
+            </div>
+            <div className='featured-image-container'>
+              <BsChevronLeft className='chevron-image left' size='2rem' />
+              <img src='images/pilot-vp.jpg' alt='notebook' />
+              <BsChevronRight className='chevron-image right' size='2rem' />
+            </div>
+          </div>
+          <div className='featured-section'>
+            <div className='featured-title'>
+              <h3>Featured Inks</h3>
+            </div>
+            <div className='featured-image-container'>
+              <BsChevronLeft className='chevron-image left' size='2rem' />
+              <img src='images/montverde-burgundy.jpg' alt='notebook' />
+              <BsChevronRight className='chevron-image right' size='2rem' />
+            </div>
+          </div>
         </div>
       </div>
-      <div className='side-bar'>
-        <div className='featured-section'>
-          <div className='featured-title'>
-            <h3>Featured Pens</h3>
-          </div>
-          <div className='featured-image-container'>
-            <BsChevronLeft className='chevron-image left' size='2rem' />
-            <img src='images/pilot-vp.jpg' alt='notebook' />
-            <BsChevronRight className='chevron-image right' size='2rem' />
-          </div>
-        </div>
-        <div className='featured-section'>
-          <div className='featured-title'>
-            <h3>Featured Inks</h3>
-          </div>
-          <div className='featured-image-container'>
-            <BsChevronLeft className='chevron-image left' size='2rem' />
-            <img src='images/montverde-burgundy.jpg' alt='notebook' />
-            <BsChevronRight className='chevron-image right' size='2rem' />
-          </div>
-        </div>
+      <div className='main-category-container'>
+        <MainCategory categories={pens} />
+        <MainCategory categories={inks} />
+        <MainCategory categories={papers} />
+        <MainCategory categories={accessories} />
+        <Brands />
       </div>
     </div>
   );
