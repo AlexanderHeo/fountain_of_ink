f2b119001786f0fcc4d0ddd83bebd768b5a43179
Alexander Heo <alexheo.webdev@gmail.com>
2021-12-21 19:41:45 -0800

    lib: colors list, with img and alt text, for pens and inks

---
+++ client/components/lib/colors.js
@@ -0,0 +1,149 @@
+export const penColors = [
+  {
+    id: 1,
+    color: 'black',
+    img: 'images/pens/color-black.jpg',
+    alt: 'black pens',
+  },
+  {
+    id: 2,
+    color: 'blue',
+    img: 'images/pens/color-blue.jpg',
+    alt: 'blue pens',
+  },
+  {
+    id: 3,
+    color: 'brown',
+    img: 'images/pens/color-brown.jpg',
+    alt: 'brown pens',
+  },
+  {
+    id: 4,
+    color: 'clear',
+    img: 'images/pens/color-clear.jpg',
+    alt: 'clear pens',
+  },
+  {
+    id: 5,
+    color: 'green',
+    img: 'images/pens/color-green.jpg',
+    alt: 'green pens',
+  },
+  {
+    id: 6,
+    color: 'orange',
+    img: 'images/pens/color-orange.jpg',
+    alt: 'orange pens',
+  },
+  {
+    id: 7,
+    color: 'pink',
+    img: 'images/pens/color-pink.jpg',
+    alt: 'pink pens',
+  },
+  {
+    id: 8,
+    color: 'purple',
+    img: 'images/pens/color-purple.jpg',
+    alt: 'purple pens',
+  },
+  {
+    id: 9,
+    color: 'red',
+    img: 'images/pens/color-red.jpg',
+    alt: 'red pens',
+  },
+  {
+    id: 10,
+    color: 'gray',
+    img: 'images/pens/color-gray.jpg',
+    alt: 'gray pens',
+  },
+  {
+    id: 11,
+    color: 'turqoise',
+    img: 'images/pens/color-turqoise.jpg',
+    alt: 'turqoise pens',
+  },
+  {
+    id: 12,
+    color: 'multi',
+    img: 'images/pens/color-multi.jpg',
+    alt: 'multi pens',
+  },
+];
+
+export const inkColors = [
+  {
+    id: 1,
+    color: 'black',
+    img: 'images/inks/ink-black.jpg',
+    alt: 'black inks',
+  },
+  {
+    id: 2,
+    color: 'blue',
+    img: 'images/inks/ink-blue.jpg',
+    alt: 'blue inks',
+  },
+  {
+    id: 3,
+    color: 'brown',
+    img: 'images/inks/ink-brown.jpg',
+    alt: 'brown inks',
+  },
+  {
+    id: 4,
+    color: 'green',
+    img: 'images/inks/ink-green.jpg',
+    alt: 'green inks',
+  },
+  {
+    id: 5,
+    color: 'gray',
+    img: 'images/inks/ink-gray.jpg',
+    alt: 'gray inks',
+  },
+  {
+    id: 6,
+    color: 'orange',
+    img: 'images/inks/ink-orange.jpg',
+    alt: 'orange inks',
+  },
+  {
+    id: 7,
+    color: 'pink',
+    img: 'images/inks/ink-pink.jpg',
+    alt: 'pink inks',
+  },
+  {
+    id: 8,
+    color: 'purple',
+    img: 'images/inks/ink-purple.jpg',
+    alt: 'purple inks',
+  },
+  {
+    id: 9,
+    color: 'red',
+    img: 'images/inks/ink-red.jpg',
+    alt: 'red inks',
+  },
+  {
+    id: 10,
+    color: 'turqoise',
+    img: 'images/inks/ink-turqoise.jpg',
+    alt: 'turqoise inks',
+  },
+  {
+    id: 11,
+    color: 'yellow',
+    img: 'images/inks/ink-yellow.jpg',
+    alt: 'yellow inks',
+  },
+  {
+    id: 12,
+    color: 'white',
+    img: 'images/inks/ink-white.jpg',
+    alt: 'white inks',
+  },
+];
